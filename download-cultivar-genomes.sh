77149577584e29db78fed74bf8ee0dbd9ad8f6a3
Clara Jegousse <clara.jegousse@gmail.com>
2021-06-18 13:30:36 +0000

    Dowload cultivar genomes

--- download-cultivar-genomes.sh
+++ download-cultivar-genomes.sh
@@ -1,25 +1,30 @@
 ##!/usr/bin/env bash
 
-esearch -db genome -query "txid1132028[Organism:exp]" |\
-  efetch -format docsum |\
-  xtract -pattern DocumentSummary \
-    -element TaxId,Organism_Name,Assembly_Accession,Status
+# get the assembly accession numbers for the cultivated strains
+cat cultivar-genomes-taxid.txt | while read -r taxid;
+do
+  esearch -db genome -query "txid"$taxid"[Organism:exp]" </dev/null |\
+    efetch -format docsum |\
+    xtract -pattern DocumentSummary \
+      -element TaxId,Organism_Name,Assembly_Accession,Status;
+  #esearch -db genome -query "txid"$taxid"[Organism:exp]"
+done > cultivar-assemblies-acc.txt
 
-# cat assemblies.txt | cut -f 3 > assemblies_acc.txt
-# cat assemblies_acc.txt | while read -r acc ; do esearch -db assembly -query $acc </dev/null \
-# | esummary \
-# | xtract -pattern DocumentSummary -element FtpPath_GenBank \
-# | while read -r url ; do
-# fname=$(echo $url | grep -o ’GCA_.*’ | sed ’s/$/_genomic.fna.gz/’) ;
-# wget "$url/$fname"
-# taxid=$(esearch -db assembly -query $acc </dev/null \
-# | esummary \
-# | xtract -pattern DocumentSummary -element Taxid)
-# echo $taxid
-# zcat $fname | sed ’s/>* /|kraken:taxid|’$taxid’ /’ > $acc.fna
-# done ; done
-# for file in GCA *. fna
-# do
-# echo $file
-# #kraken2-build --add-to-library $file --db $DBNAME
-# done
+# download the genomes
+cat cultivar-assemblies-acc.txt | cut -f 3 | while read -r acc ; do
+  echo $acc
+  esearch -db assembly -query $acc </dev/null \
+    | esummary \
+    | xtract -pattern DocumentSummary -element FtpPath_GenBank \
+    | while read -r url ;
+      do
+      echo "$url/$fname"
+      fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+      echo $fname
+      wget "$url/$fname"
+      zcat $fname > $acc.fna
+    done
+done
+
+# clean up
+rm -f *.fna.gz; rm -f index.html*

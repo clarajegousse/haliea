e27e4dfa895f3d33882a067656de93dd4e965bce
Clara Jegousse <clara.jegousse@gmail.com>
2021-06-21 12:25:57 +0000

    anvi-run-hmms

---
+++ 03-anvi-run-hmms.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=hmm
+#SBATCH -p normal
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+echo $HOSTNAME
+
+# to insure work with python3
+source /users/home/cat3/.bashrc
+
+# activate environment
+conda activate anvio-master
+
+# go to working directory
+WD=/users/home/cat3/projects/haliea
+cd $WD
+
+CONTIGSDB="data/cultivar-genomes/*.db"
+for cdb in $CONTIGSDB
+do
+  echo "Processing $cdb ..."
+  anvi-run-hmms -c $cdb -T 8 --also-scan-trnas 
+done

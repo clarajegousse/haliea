8f21a9c41d45c581fcefdc507005b15884102839
Clara Jegousse <clara.jegousse@gmail.com>
2021-06-22 16:40:42 +0000

    anvi-gen-contigs-database

---
+++ 02-make-haliea-genomes-anvio-db.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=cultivar-db
+#SBATCH -p normal
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+echo $HOSTNAME
+
+# to insure work with python3
+source /users/home/cat3/.bashrc
+
+# activate environment
+conda activate anvio-master
+
+# go to working directory
+WD=/users/home/cat3/projects/haliea
+cd $WD
+
+FILES="data/haliea-genomes/*.fna"
+for f in $FILES
+do
+  echo "Processing $f file..."
+  spname=$(echo $f | cut -f 3 -d "/" | cut -f 1 -d ".")
+  anvi-script-reformat-fasta $f \
+    -o data/haliea-genomes/$spname.fa \
+    --simplify-names
+
+  anvi-gen-contigs-database -f data/haliea-isolates/$spname.fa \
+  -o data/haliea-genomes/$spname.db -T 8
+
+done

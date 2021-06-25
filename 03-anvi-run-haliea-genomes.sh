8646f33d733e7ccde22053798debdb65d30e90f3
Clara Jegousse <clara.jegousse@gmail.com>
2021-06-25 13:00:18 +0000

    anvi-run

---
+++ 03-anvi-run-haliea-genomes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=anvi-run
+#SBATCH -p normal
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+echo $HOSTNAME
+
+# to insure work with python3
+source /users/home/cat3/.bashrc
+
+# activate environment
+conda activate anvio-master
+
+# go to working directory
+WD=/users/home/cat3/projects/haliea
+cd $WD
+
+anvi-run-hmms -c $WD/HALIEA-CONTIGS.db -T 8 --also-scan-trnas
+anvi-run-pfams -c $WD/HALIEA-CONTIGS.db -T 8
+anvi-run-ncbi-cogs -c $WD/HALIEA-CONTIGS.db -T 8  --sensitive

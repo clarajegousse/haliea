6f50b459a54fb261df38ada380e8b5f983a89c83
Clara Jegousse <clara.jegousse@gmail.com>
2021-07-01 15:05:42 +0000

    cleanup

---
+++ 00-download-halieae-genomes.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+WD=/users/home/cat3/projects/haliea/data
+cd $WD
+
+esearch -db taxonomy -query "txid1706372[Subtree]" |\
+ efetch -format xml |\
+ xtract -pattern Taxon \
+ 	-element TaxId,ScientificName > halieacaea-taxid.txt
+
+# get the assembly accession numbers for the cultivated strains according to the DMSZ
+cat halieacaea-taxid.txt | cut -f 1 | while read -r taxid;
+do
+  esearch -db genome -query "txid"$taxid"[Organism:exp]" </dev/null |\
+    efetch -format docsum |\
+    xtract -pattern DocumentSummary \
+      -element TaxId,Organism_Name,Assembly_Accession,Status;
+  #esearch -db genome -query "txid"$taxid"[Organism:exp]"
+done > halieaceae-genomes-assemblies-acc.txt
+
+# download the genomes
+mkdir -p $WD/halieaceae-genomes
+cat halieaceae-genomes-assemblies-acc.txt | grep "GCA\_" | grep "Complete" | cut -f 3 | while read -r acc ; do
+  echo $acc
+  esearch -db assembly -query $acc </dev/null \
+    | esummary \
+    | xtract -pattern DocumentSummary -element FtpPath_GenBank \
+    | while read -r url ;
+      do
+      #echo "$url/$fname"
+      fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+      #echo $fname
+      wget "$url/$fname"
+      spname=$(zcat $fname | grep ">" | head -1 | cut -f 2,3 -d " " | sed 's/\ /-/i')
+      #echo $spname
+      zcat $fname > halieaceae-genomes/$spname.fna
+      rm -f $fname
+    done
+done
+
+# clean up
+rm -f *.fna.gz; rm -f index.html*
--- 00-download-marine-metagenomes.sh
+++ 00-download-marine-metagenomes.sh
@@ -18,14 +18,3 @@ do
 	echo $url
     wget ftp://$url
 done
-
-#
-# esearch -db sra -query ERR2762177 | efetch --format docsum
-# esearch -db sra -query ERR2762177 \
-# | esummary \
-# | xtract -pattern DocumentSummary -element FtpPath_GenBank \
-# | while read -r url ;
-#
-# prefetch ERR2762177
-
-# scp -i ~/.ssh/id_rsa_ihpc /Users/Clara/Desktop/data/ERR2762181_1.fastq.gz cat3@garpur.ihpc.hi.is:/users/work/cat3/projects/haliea/data/metagenomes/

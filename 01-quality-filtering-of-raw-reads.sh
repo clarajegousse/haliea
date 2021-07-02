3a1aa55daf6d00758e9f5c39297a7a26ab8b9419
Clara Jegousse <clara.jegousse@gmail.com>
2021-07-02 13:57:56 +0000

    gzip

--- 01-quality-filtering-of-raw-reads.sh
+++ 01-quality-filtering-of-raw-reads.sh
@@ -26,4 +26,5 @@ do
 	echo $sample
     iu-filter-quality-minoche $WD/01-qc-tara-metagenomes/$sample.ini --ignore-deflines
 	gzip $WD/00-tara-metagenomes/$sample-QUALITY_PASSED_R1.fastq
+	gzip $WD/00-tara-metagenomes/$sample-QUALITY_PASSED_R2.fastq
 done

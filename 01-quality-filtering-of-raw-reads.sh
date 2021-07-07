b81d69010f225d3ade33c1a49a2ae25fb2591db5
Clara Jegousse <clara.jegousse@gmail.com>
2021-07-07 09:31:38 +0000

    new qc

--- 01-quality-filtering-of-raw-reads.sh
+++ 01-quality-filtering-of-raw-reads.sh
@@ -19,9 +19,9 @@ conda activate anvio-master
 WD=/users/home/cat3/projects/haliea
 cd $WD/00-tara-metagenomes
 
-iu-gen-configs samples2.txt -o $WD/01-qc-tara-metagenomes
+iu-gen-configs samples-subset.txt -o $WD/01-qc-tara-metagenomes
 
-for sample in `awk '{print $1}' samples2.txt`
+for sample in `awk '{print $1}' samples-subset.txt`
 do
     if [ "$sample" == "sample" ]; then continue; fi
 	echo $sample

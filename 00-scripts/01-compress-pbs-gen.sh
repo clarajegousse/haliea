65cebe9e59846b4b0de01063bc6b06c06900b620
Clara Jegousse <clara.jegousse@gmail.com>
2021-07-11 11:00:09 +0000

    fix pbs

--- 00-scripts/01-compress-pbs-gen.sh
+++ 00-scripts/01-compress-pbs-gen.sh
@@ -15,20 +15,20 @@ do
     if [ "$sample" == "sample" ]; then continue; fi
 	echo $sample
 	echo '''#!/bin/bash
-	#SBATCH --job-name=gzip-'$sample'
-	#SBATCH -p normal
-	#SBATCH --time=3-00:00:00
-	#SBATCH --mail-type=ALL
-	#SBATCH --mail-user=cat3@hi.is
-	#SBATCH -N 1
-	#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=gzip-'$sample'
+#SBATCH -p normal
+#SBATCH --time=3-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
 
-	echo $HOSTNAME
-	source /users/home/cat3/.bashrc
-	conda activate anvio-master
-	WD=/users/home/cat3/projects/haliea
-	cd $WD/00-tara-metagenomes
-	gzip $WD/01-qc-tara-metagenomes/'$sample'-QUALITY_PASSED_R1.fastq
-	gzip $WD/01-qc-tara-metagenomes/'$sample'-QUALITY_PASSED_R2.fastq
-	''' > $WD/01-qc-tara-metagenomes/$sample'-gzip-pbs.sh'
+echo $HOSTNAME
+source /users/home/cat3/.bashrc
+conda activate anvio-master
+WD=/users/home/cat3/projects/haliea
+cd $WD/00-tara-metagenomes
+gzip $WD/01-qc-tara-metagenomes/'$sample'-QUALITY_PASSED_R1.fastq
+gzip $WD/01-qc-tara-metagenomes/'$sample'-QUALITY_PASSED_R2.fastq
+''' > $WD/01-qc-tara-metagenomes/$sample'-gzip-pbs.sh'
 done

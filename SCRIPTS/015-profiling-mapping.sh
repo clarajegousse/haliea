811ed868177b26af2814266c66c64667815f4dc9
Clara Jegousse <clara.jegousse@gmail.com>
2021-07-13 14:03:24 +0000

    profiles

---
+++ SCRIPTS/015-profiling-mapping.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name=profiling
+#SBATCH -p normal
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+
+echo $HOSTNAME
+
+# to insure work with python3
+source /users/home/cat3/.bashrc
+
+# activate environment
+conda activate anvio-master
+
+# go to working directory
+WD=/users/home/cat3/projects/haliea
+cd $WD
+
+mkdir -p $WD/PROFILES
+
+for sample in `awk '{print $1}' $WD/RAW-READS/samples.txt`
+do
+    if [ "$sample" == "sample" ]; then continue; fi
+
+    anvi-profile -c SAR11-CONTIGS.db \
+                 -i $WD'/DATA-SAMPLES/'$sample'.bam' \
+                 --profile-AA-frequencies \
+                 --num-threads 16 \
+                 -o $WD'/PROFILES'/$sample -W
+done
